f22e891aac2141cbe7d3429d0d8ee23e3bd9adae
Sam Steele <sam.steele@gmail.com>
2006-11-25 17:56:46 +0000

    remove old TextWindow API

--- include/status.h
+++ include/status.h
@@ -46,28 +46,4 @@ void take_time(int count);
 void give_time(int count);
 void draw_keys();
 void draw_hud_ingame();
-
-class TextWindow {
-public:
-	TextWindow(Tiki::GL::ConsoleText *c,std::string title,std::string text);
-
-	char *getLabel() { return retlbl; }
-	void doMenu();
-	void pc_box(int x, int y,int w,int h,int fg,int bg);
-	void draw_shadow(int x, int y);
-
-private:
-	// HID input callback.
-		static void hidCallback(const Tiki::Hid::Event & evt, void * data);
-	virtual void processHidEvent(const Tiki::Hid::Event & evt);
-	int m_hidCookie;
-	
-	int y,loop,maxy,dirty;
-  char txt[150][50];
-  char lbl[50][20];
-  int lblidx[150];
-  int q,lblcnt;	
-	char retlbl[20];
-	Tiki::GL::ConsoleText *console;
-};
 #endif
