661b1cf34923f8cc2bd1d32b076b1fe0847a1048
Sam Steele <sam.steele@gmail.com>
2006-12-02 02:24:54 +0000

    Add GPL header

--- src/os.mm
+++ src/os.mm
@@ -1,10 +1,21 @@
-//
-//  os.mm
-//  DreamZZT
-//
-//  Created by Sam Steele on 11/25/06.
-//  Copyright 2006 __MyCompanyName__. All rights reserved.
-//
+/* os.mm - OS hooks for Mac OS X
+ * Copyright (c) 2000-2006 Sam Steele
+ *
+ * This file is part of DreamZZT.
+ *
+ * DreamZZT is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * DreamZZT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */ 
 
 #include <Cocoa/Cocoa.h>
 #include <string>
