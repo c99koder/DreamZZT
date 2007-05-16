935847401100df510ed5de62641f1fa3c2b7a322
Sam Steele <sam.steele@gmail.com>
2007-05-16 23:15:01 +0000

    Import libcurl's internal base64 function

--- src/bugreport.cpp
+++ src/bugreport.cpp
@@ -28,9 +28,82 @@ using namespace Tiki;
 #include "http.h"
 #include "bugreport.h"
 
-extern "C" {
-int Curl_base64_encode(const void *inp, int insize, char **outptr);
+/* ---- Base64 Encoding --- */
+static char table64[]=
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  
+/*
+ * base64_encode()
+ *
+ * Returns the length of the newly created base64 string. The third argument
+ * is a pointer to an allocated area holding the base64 data. If something
+ * went wrong, -1 is returned.
+ *
+ * Copyright (C) 2001, Andrew Francis and Daniel Stenberg
+ */
+int base64_encode(const void *inp, int insize, char **outptr)
+{
+  unsigned char ibuf[3];
+  unsigned char obuf[4];
+  int i;
+  int inputparts;
+  char *output;
+  char *base64data;
+
+  char *indata = (char *)inp;
+
+  if(0 == insize)
+    insize = strlen(indata);
+
+  base64data = output = (char*)malloc(insize*4/3+4);
+  if(NULL == output)
+    return -1;
+
+  while(insize > 0) {
+    for (i = inputparts = 0; i < 3; i++) { 
+      if(*indata) {
+        inputparts++;
+        ibuf[i] = *indata;
+        indata++;
+        insize--;
+      }
+      else
+        ibuf[i] = 0;
+    }
+                       
+    obuf [0] = (ibuf [0] & 0xFC) >> 2;
+    obuf [1] = ((ibuf [0] & 0x03) << 4) | ((ibuf [1] & 0xF0) >> 4);
+    obuf [2] = ((ibuf [1] & 0x0F) << 2) | ((ibuf [2] & 0xC0) >> 6);
+    obuf [3] = ibuf [2] & 0x3F;
+
+    switch(inputparts) {
+    case 1: /* only one byte read */
+      sprintf(output, "%c%c==", 
+              table64[obuf[0]],
+              table64[obuf[1]]);
+      break;
+    case 2: /* two bytes read */
+      sprintf(output, "%c%c%c=", 
+              table64[obuf[0]],
+              table64[obuf[1]],
+              table64[obuf[2]]);
+      break;
+    default:
+      sprintf(output, "%c%c%c%c", 
+              table64[obuf[0]],
+              table64[obuf[1]],
+              table64[obuf[2]],
+              table64[obuf[3]] );
+      break;
+    }
+    output += 4;
+  }
+  *output=0;
+  *outptr = base64data; /* make it return the actual data memory */
+
+  return strlen(base64data); /* return the length of the new data */
 }
+/* ---- End of Base64 Encoding ---- */
 
 TracBug::TracBug() {
 	m_num = -1;
@@ -114,7 +187,7 @@ void TracBug::attach(std::string filename, std::string description) {
 	char *buf = (char *)malloc(f.total());
 	char *fileData;
 	f.read(buf,f.total());
-	Curl_base64_encode(buf,f.total(),&fileData);
+	base64_encode(buf,f.total(),&fileData);
 	f.close();
 
 	std::stringstream attach;
